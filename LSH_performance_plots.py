8dc1eb195f3e6f4899a9b525776b111ef4198d9d
539908 <539908dg@student.eur.nl>
2023-12-13 22:01:19 +0100

    update labels of plot

--- LSH_performance_plots.py
+++ LSH_performance_plots.py
@@ -1,23 +1,34 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_excel("results_LSH.xlsx", index_col=0)
+LSH_df_list = []
+for i in range(20):
+    LSH_df = pd.read_excel('results_LSH_bootstrap_' + str(i) + '.xlsx', index_col=0)
+    LSH_df_list.append(LSH_df)
+
+
+mean_LSH = pd.concat(LSH_df_list).groupby(level=0).mean()
 plot_list_y = ["pair_quality", "pair_completeness", "F1_star"]
 
 def plot_vs_fraction(row):
-    y = df.loc[row]
-    x = df.loc["Fraction_of_comparisons"]
+    y = mean_LSH.loc[row]
+    x = mean_LSH.loc["Fraction_of_comparisons"]
     plt.scatter(x, y, color='red')
 
     plt.plot(x, y, marker='o', linestyle='-', color='red')
-    plt.xlabel('Fraction_of_comparisons')
-    plt.ylabel(row)
+    plt.xlabel('fraction of comparisons')
+    if row == 'pair_quality':
+        plt.ylabel('pair quality')
+    elif row == 'pair_completeness':
+        plt.ylabel('pair completeness')
+    else:
+        plt.ylabel('f1 star')
 
-    plt.title('Scatter plot met gesmoothe lijn')
+    plt.title('')
     plt.show()
 
-    # Save the plot
-    plt.savefig( row + '.png')
+
+    plt.savefig(row + '.png')
 
 for e in plot_list_y:
    plot_vs_fraction(e)
