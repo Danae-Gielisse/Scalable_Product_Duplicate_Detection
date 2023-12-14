b13de6c4e56e5b24aeda5075c40745d88bbdcbc8
539908 <539908dg@student.eur.nl>
2023-12-14 21:46:22 +0100

    add the less cleaning part

--- find_best_parameters.py
+++ find_best_parameters.py
@@ -1,8 +1,13 @@
 import pandas as pd
 
+less_cleaning = False
+cleaning_string = ''
+if less_cleaning:
+    cleaning_string = '_less_cleaning'
+
 bootstrap_list = []
 for i in range(20):
-    bootstrap = pd.read_excel('results_F1_bootstrap_' + str(i) + '.xlsx', index_col=0)
+    bootstrap = pd.read_excel('results_F1_bootstrap_' + str(i) + cleaning_string + '.xlsx', index_col=0)
     bootstrap_list.append(bootstrap)
 
 
@@ -15,3 +20,6 @@ max_value = mean_F1.values.max()
 print("The best threshold is: " + str(max_t))
 print("The best b is: " + str(max_b))
 print("The max F1_value is: " + str(max_value))
+
+row_with_best_threshold = mean_F1.loc[max_t]
+row_with_best_threshold.to_excel('row_with_best_threshold' + cleaning_string + '.xlsx', index_label='F1_score')
